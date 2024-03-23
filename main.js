f583e1a14f22c1912b403c544a998ff9845cd6fe
Hung Hay Hat <diegocoshung19@gmail.com>
2024-03-23 13:50:16 +0700

    create myFilter method

--- main.js
+++ main.js
@@ -330,7 +330,38 @@ Array.prototype.reduce2 = function(callback, result){
         callback(this)
     }
 }
-  
+
+// Create myFilter method()
+
+Array.prototype.myFilter = function(cb) {
+    var a = []
+     for (var index in this){
+         if(this.hasOwnProperty(index)){
+            var result = cb(this[index], index, this)
+            if (result) 
+                a.push(this[index])
+         }
+     }
+     return a;
+ }
+ 
+ const numbers = [1, 2, 3, 4];
+ 
+ console.log(numbers.myFilter(function (number) {
+     return number % 2 === 0;
+ }));
+ //  Output: [2, 4]
+ 
+ console.log(numbers.myFilter(function (number, index) {
+     return index % 2 === 0;
+ }));
+ //  Output: [1, 3]
+ 
+ console.log(numbers.myFilter(function (number, index, array) {
+     return array.length % 2 === 0;
+ }));
+ //  Output: [1, 2, 3, 4]
+
   
   
   
