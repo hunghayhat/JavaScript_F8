ad4ca4b4a0aee6657c70461ef244faf2651188dc
Hung Hay Hat <diegocoshung19@gmail.com>
2024-03-24 15:20:21 +0700

    HTML DOM

--- main.js
+++ main.js
@@ -100,7 +100,7 @@
 //     }
 //     console.log(sum)
 //     return sum
-   
+
 // }
 
 
@@ -123,7 +123,7 @@
 //         name: 'dragon ball',
 //         price: 50
 //     },
-  
+
 // ]
 // var list = animes.every(function(anime) {
 //     return anime.price === 50
@@ -169,8 +169,7 @@
 // Output: [{ name: 'Bóng rổ, like: 6 }, { name: 'Bóng đá, like: 10 }]
 
 
-var sports = [
-    {
+var sports = [{
         name: 'Bơi lội',
         gold: 11
     },
@@ -187,16 +186,17 @@ var sports = [
         gold: 5
     },
 ]
-function getTotalGold(arr){
-    var a = arr.reduce(function(acc, cur){
+
+function getTotalGold(arr) {
+    var a = arr.reduce(function (acc, cur) {
         return acc + cur.gold
-    },0);
+    }, 0);
     return a
 }
 
 var a = [1, 2, 3, [4, 5], 6, [7, 8, 9]]
 
-var flatArray = a.reduce(function(accum, cur){
+var flatArray = a.reduce(function (accum, cur) {
     return accum.concat(cur)
 }, [])
 
@@ -208,163 +208,196 @@ console.log(getTotalGold(sports)) // Output: 23
 
 
 
-var watchList = [
-    {
-      "Title": "Inception",
-      "Year": "2010",
-      "Rated": "PG-13",
-      "Released": "16 Jul 2010",
-      "Runtime": "148 min",
-      "Genre": "Action, Adventure, Crime",
-      "Director": "Christopher Nolan",
-      "Writer": "Christopher Nolan",
-      "Actors": "Leonardo DiCaprio, Joseph Gordon-Levitt, Elliot Page, Tom Hardy",
-      "Plot": "A thief, who steals corporate secrets through use of dream-sharing technology, is given the inverse task of planting an idea into the mind of a CEO.",
-      "Language": "English, Japanese, French",
-      "Country": "USA, UK",
-      "imdbRating": "8.8",
-      "imdbVotes": "1,446,708",
-      "imdbID": "tt1375666",
-      "Type": "movie",
+var watchList = [{
+        "Title": "Inception",
+        "Year": "2010",
+        "Rated": "PG-13",
+        "Released": "16 Jul 2010",
+        "Runtime": "148 min",
+        "Genre": "Action, Adventure, Crime",
+        "Director": "Christopher Nolan",
+        "Writer": "Christopher Nolan",
+        "Actors": "Leonardo DiCaprio, Joseph Gordon-Levitt, Elliot Page, Tom Hardy",
+        "Plot": "A thief, who steals corporate secrets through use of dream-sharing technology, is given the inverse task of planting an idea into the mind of a CEO.",
+        "Language": "English, Japanese, French",
+        "Country": "USA, UK",
+        "imdbRating": "8.8",
+        "imdbVotes": "1,446,708",
+        "imdbID": "tt1375666",
+        "Type": "movie",
     },
     {
-      "Title": "Interstellar",
-      "Year": "2014",
-      "Rated": "PG-13",
-      "Released": "07 Nov 2014",
-      "Runtime": "169 min",
-      "Genre": "Adventure, Drama, Sci-Fi",
-      "Director": "Christopher Nolan",
-      "Writer": "Jonathan Nolan, Christopher Nolan",
-      "Actors": "Ellen Burstyn, Matthew McConaughey, Mackenzie Foy, John Lithgow",
-      "Plot": "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.",
-      "Language": "English",
-      "Country": "USA, UK",
-      "imdbRating": "8.6",
-      "imdbVotes": "910,366",
-      "imdbID": "tt0816692",
-      "Type": "movie",
+        "Title": "Interstellar",
+        "Year": "2014",
+        "Rated": "PG-13",
+        "Released": "07 Nov 2014",
+        "Runtime": "169 min",
+        "Genre": "Adventure, Drama, Sci-Fi",
+        "Director": "Christopher Nolan",
+        "Writer": "Jonathan Nolan, Christopher Nolan",
+        "Actors": "Ellen Burstyn, Matthew McConaughey, Mackenzie Foy, John Lithgow",
+        "Plot": "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.",
+        "Language": "English",
+        "Country": "USA, UK",
+        "imdbRating": "8.6",
+        "imdbVotes": "910,366",
+        "imdbID": "tt0816692",
+        "Type": "movie",
     },
     {
-      "Title": "The Dark Knight",
-      "Year": "2008",
-      "Rated": "PG-13",
-      "Released": "18 Jul 2008",
-      "Runtime": "152 min",
-      "Genre": "Action, Adventure, Crime",
-      "Director": "Christopher Nolan",
-      "Writer": "Jonathan Nolan (screenplay), Christopher Nolan (screenplay), Christopher Nolan (story), David S. Goyer (story), Bob Kane (characters)",
-      "Actors": "Christian Bale, Heath Ledger, Aaron Eckhart, Michael Caine",
-      "Plot": "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, the caped crusader must come to terms with one of the greatest psychological tests of his ability to fight injustice.",
-      "Language": "English, Mandarin",
-      "Country": "USA, UK",
-      "imdbRating": "9.0",
-      "imdbVotes": "1,652,832",
-      "imdbID": "tt0468569",
-      "Type": "movie",
+        "Title": "The Dark Knight",
+        "Year": "2008",
+        "Rated": "PG-13",
+        "Released": "18 Jul 2008",
+        "Runtime": "152 min",
+        "Genre": "Action, Adventure, Crime",
+        "Director": "Christopher Nolan",
+        "Writer": "Jonathan Nolan (screenplay), Christopher Nolan (screenplay), Christopher Nolan (story), David S. Goyer (story), Bob Kane (characters)",
+        "Actors": "Christian Bale, Heath Ledger, Aaron Eckhart, Michael Caine",
+        "Plot": "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, the caped crusader must come to terms with one of the greatest psychological tests of his ability to fight injustice.",
+        "Language": "English, Mandarin",
+        "Country": "USA, UK",
+        "imdbRating": "9.0",
+        "imdbVotes": "1,652,832",
+        "imdbID": "tt0468569",
+        "Type": "movie",
     },
     {
-      "Title": "Batman Begins",
-      "Year": "2005",
-      "Rated": "PG-13",
-      "Released": "15 Jun 2005",
-      "Runtime": "140 min",
-      "Genre": "Action, Adventure",
-      "Director": "Christopher Nolan",
-      "Writer": "Bob Kane (characters), David S. Goyer (story), Christopher Nolan (screenplay), David S. Goyer (screenplay)",
-      "Actors": "Christian Bale, Michael Caine, Liam Neeson, Katie Holmes",
-      "Plot": "After training with his mentor, Batman begins his fight to free crime-ridden Gotham City from the corruption that Scarecrow and the League of Shadows have cast upon it.",
-      "Language": "English, Urdu, Mandarin",
-      "Country": "USA, UK",
-      "imdbRating": "8.3",
-      "imdbVotes": "972,584",
-      "imdbID": "tt0372784",
-      "Type": "movie",
+        "Title": "Batman Begins",
+        "Year": "2005",
+        "Rated": "PG-13",
+        "Released": "15 Jun 2005",
+        "Runtime": "140 min",
+        "Genre": "Action, Adventure",
+        "Director": "Christopher Nolan",
+        "Writer": "Bob Kane (characters), David S. Goyer (story), Christopher Nolan (screenplay), David S. Goyer (screenplay)",
+        "Actors": "Christian Bale, Michael Caine, Liam Neeson, Katie Holmes",
+        "Plot": "After training with his mentor, Batman begins his fight to free crime-ridden Gotham City from the corruption that Scarecrow and the League of Shadows have cast upon it.",
+        "Language": "English, Urdu, Mandarin",
+        "Country": "USA, UK",
+        "imdbRating": "8.3",
+        "imdbVotes": "972,584",
+        "imdbID": "tt0372784",
+        "Type": "movie",
     },
     {
-      "Title": "Avatar",
-      "Year": "2009",
-      "Rated": "PG-13",
-      "Released": "18 Dec 2009",
-      "Runtime": "162 min",
-      "Genre": "Action, Adventure, Fantasy",
-      "Director": "James Cameron",
-      "Writer": "James Cameron",
-      "Actors": "Sam Worthington, Zoe Saldana, Sigourney Weaver, Stephen Lang",
-      "Plot": "A paraplegic marine dispatched to the moon Pandora on a unique mission becomes torn between following his orders and protecting the world he feels is his home.",
-      "Language": "English, Spanish",
-      "Country": "USA, UK",
-      "imdbRating": "7.9",
-      "imdbVotes": "876,575",
-      "imdbID": "tt0499549",
-      "Type": "movie",
+        "Title": "Avatar",
+        "Year": "2009",
+        "Rated": "PG-13",
+        "Released": "18 Dec 2009",
+        "Runtime": "162 min",
+        "Genre": "Action, Adventure, Fantasy",
+        "Director": "James Cameron",
+        "Writer": "James Cameron",
+        "Actors": "Sam Worthington, Zoe Saldana, Sigourney Weaver, Stephen Lang",
+        "Plot": "A paraplegic marine dispatched to the moon Pandora on a unique mission becomes torn between following his orders and protecting the world he feels is his home.",
+        "Language": "English, Spanish",
+        "Country": "USA, UK",
+        "imdbRating": "7.9",
+        "imdbVotes": "876,575",
+        "imdbID": "tt0499549",
+        "Type": "movie",
     }
-  ];
-  
+];
 
-function calculateRating(watchList){
-        var a = watchList.filter(function(director){
+
+function calculateRating(watchList) {
+    var a = watchList.filter(function (director) {
         return director.Director === "Christopher Nolan"
-      }) 
-    var imdb = a.reduce(function(accum, cur){
-        return accum + cur.imdbRating*1
+    })
+    var imdb = a.reduce(function (accum, cur) {
+        return accum + cur.imdbRating * 1
     }, 0)
     return (imdb / (a.length))
 }
 
-  // Expected results
-  console.log(calculateRating(watchList)); // Output: 8.675
-  
+// Expected results
+console.log(calculateRating(watchList)); // Output: 8.675
+
 // Tạo phương thức reduce()
 
 const number = [1, 2, 3, 4, 5]
 
-const result = number.reduce(function(total,current){
+const result = number.reduce(function (total, current) {
     return total + current
 })
 
 console.log(result)
 
-Array.prototype.reduce2 = function(callback, result){
-    for (let i = 0; i < this.length; i++){
+Array.prototype.reduce2 = function (callback, result) {
+    for (let i = 0; i < this.length; i++) {
         callback(this)
     }
 }
 
 // Create myFilter method()
 
-Array.prototype.myFilter = function(cb) {
+Array.prototype.myFilter = function (cb) {
     var a = []
-     for (var index in this){
-         if(this.hasOwnProperty(index)){
+    for (var index in this) {
+        if (this.hasOwnProperty(index)) {
             var result = cb(this[index], index, this)
-            if (result) 
+            if (result)
                 a.push(this[index])
-         }
-     }
-     return a;
- }
- 
- const numbers = [1, 2, 3, 4];
- 
- console.log(numbers.myFilter(function (number) {
-     return number % 2 === 0;
- }));
- //  Output: [2, 4]
- 
- console.log(numbers.myFilter(function (number, index) {
-     return index % 2 === 0;
- }));
- //  Output: [1, 3]
- 
- console.log(numbers.myFilter(function (number, index, array) {
-     return array.length % 2 === 0;
- }));
- //  Output: [1, 2, 3, 4]
+        }
+    }
+    return a;
+}
 
-  
-  
-  
-  
+const numbers = [1, 2, 3, 4];
+
+console.log(numbers.myFilter(function (number) {
+    return number % 2 === 0;
+}));
+//  Output: [2, 4]
+
+console.log(numbers.myFilter(function (number, index) {
+    return index % 2 === 0;
+}));
+//  Output: [1, 3]
+
+console.log(numbers.myFilter(function (number, index, array) {
+    return array.length % 2 === 0;
+}));
+//  Output: [1, 2, 3, 4]
 
 
+
+// create mySome method
+Array.prototype.mySome = function (cb) {
+    for(var i in this){
+        if (this.hasOwnProperty(i)){
+            if(cb(this[i], i, this))
+            return true
+        }
+    }
+    return false
+}
+
+const numberss = [1, 3, 3, 5];
+
+console.log(numberss.mySome(function (number) {
+    return number % 2 === 0;
+}));
+// Output: false
+
+console.log(numberss.mySome(function (number, index) {
+    return index % 2 === 0;
+}));
+// Output: true
+
+console.log(numberss.mySome(function (number, index, array) {
+    return array.length % 2 === 0;
+}));
+//  Output: true
+
+
+// HTML DOM: 
+// GetElementBy Id, ClassName, TagName
+var headingNodes = document.querySelector(".heading")
+console.log(headingNodes)
+
+console.log(document.forms.testForm)
+
+// Lọc các thẻ a có thuộc tính "name"
+console.log(document.anchors)
